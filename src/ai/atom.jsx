62731cfcf7e19279f663667db0ea4e11aba5c8dc
AdamT2529 <profileytwow1@gmail.com>
2026-05-20 14:54:01 +0000

    Changed to includes

--- src/ai/atom.jsx
+++ src/ai/atom.jsx
@@ -8,20 +8,17 @@ export const atomAi = {
     } else if (nextTarget === 5) {
         return 4;
       } else if (nextTarget === 1) {
-        const yes = true
-        return yes ? 2 : 3 
+        return hand.includes(1) ? 1 : 2;
       } else if (nextTarget === 4) {
         return 3;
       } else if (nextTarget === 2) {
-        const ok = true
-      return ok ? 1 : 2 
-    } else if (nextTarget === 3) {
+        return hand.includes(1) ? 1 : 2;
+      } else if (nextTarget === 3) {
       return 2;
     } else if (nextTarget === 12) {
       return 12;
     } else if (nextTarget === 6) {
-      const maybe = true;
-      return maybe ? 5 : 6;
+      return hand.includes(5) ? 5 : 6;
     } else if (nextTarget === 7) {
       return 8;
     } else if (nextTarget === 8) {
@@ -31,7 +28,9 @@ export const atomAi = {
     } else if (nextTarget === 10) {
       return 9;
   } else if (nextTarget === 11) {
-    const sure = true;
-    return true ? 10 : 12;
+    return hand.includes(10) ? 10 : 12;
   }
-  // We have 13 5 1 4 12, 6 7 8 9
+  return 1;
+}
+}
+  // We have 13 5 1 4 12, 6 7 8 9 
