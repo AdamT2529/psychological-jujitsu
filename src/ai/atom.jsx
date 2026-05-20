a7f199710a692fd98d8a39bedd2f6b63cadd614b
AdamT2529 <profileytwow1@gmail.com>
2026-05-20 13:57:49 +0000

    Created the base for the atom AI

---
+++ src/ai/atom.jsx
@@ -0,0 +1,9 @@
+export const atomAi = {
+  name: "atom", 
+  // icon: "https://www.sciencefacts.net/atom-2.html", // an image link
+  getNextCard: (hand, targets, opponentPlays) => {
+    let nextTarget = targets[targets.length - 1];
+    
+    }
+  },
+};
