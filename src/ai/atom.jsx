f64b6d661de81010dc2af256270fe2b66190ad8a
AdamT2529 <profileytwow1@gmail.com>
2026-05-20 14:44:04 +0000

    Finished the parts not sure if its good but I got confued half way through so lets see

--- src/ai/atom.jsx
+++ src/ai/atom.jsx
@@ -12,8 +12,26 @@ export const atomAi = {
         return yes ? 2 : 3 
       } else if (nextTarget === 4) {
         return 3;
-      //} else if (nextTarget === ) {
-      }
-    }
+      } else if (nextTarget === 2) {
+        const ok = true
+      return ok ? 1 : 2 
+    } else if (nextTarget === 3) {
+      return 2;
+    } else if (nextTarget === 12) {
+      return 12;
+    } else if (nextTarget === 6) {
+      const maybe = true;
+      return maybe ? 5 : 6;
+    } else if (nextTarget === 7) {
+      return 8;
+    } else if (nextTarget === 8) {
+      return 7;
+    } else if (nextTarget === 9) {
+      return 10;
+    } else if (nextTarget === 10) {
+      return 9;
+  } else if (nextTarget === 11) {
+    const sure = true;
+    return true ? 10 : 12;
   }
-}
+  // We have 13 5 1 4 12, 6 7 8 9
