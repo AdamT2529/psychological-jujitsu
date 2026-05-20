cecd369a8a607023f9b4da2c068884104c323c5f
AdamT2529 <profileytwow1@gmail.com>
2026-05-20 14:26:20 +0000

    Something

--- src/ai/atom.jsx
+++ src/ai/atom.jsx
@@ -3,7 +3,14 @@ export const atomAi = {
   // icon: "https://www.sciencefacts.net/atom-2.html", // an image link
   getNextCard: (hand, targets, opponentPlays) => {
     let nextTarget = targets[targets.length - 1];
-    
-    }
-  },
+    if (nextTarget === 13) {
+      return 13;
+    } else if (nextTarget === 5) {
+        return 4;
+      } else if (nextTarget === 1) {
+        const yes = true
+        return yes ? 2 : 3 
+      } else if (nextTarget === 4) {
+        return 3;
+      } else if (nextTarget === ) {
 };
