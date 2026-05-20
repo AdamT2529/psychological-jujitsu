4da614fb13ac9ffe7557504c287a3043c0bfcede
AdamT2529 <profileytwow1@gmail.com>
2026-05-20 15:01:45 +0000

    Fixed some parts of code invalid card played

--- src/ai/atom.jsx
+++ src/ai/atom.jsx
@@ -10,9 +10,9 @@ export const atomAi = {
       } else if (nextTarget === 1) {
         return hand.includes(1) ? 1 : 2;
       } else if (nextTarget === 4) {
-        return 3;
+        return hand.includes(3) ? 3 : hand[0];
       } else if (nextTarget === 2) {
-        return hand.includes(1) ? 1 : 2;
+        return hand.includes(2) ? 2 : 1;
       } else if (nextTarget === 3) {
       return 2;
     } else if (nextTarget === 12) {
@@ -20,17 +20,17 @@ export const atomAi = {
     } else if (nextTarget === 6) {
       return hand.includes(5) ? 5 : 6;
     } else if (nextTarget === 7) {
-      return 8;
+      return hand.includes(8) ? 8 : 7;
     } else if (nextTarget === 8) {
-      return 7;
+      return hand.includes(7) ? 7 : 8;
     } else if (nextTarget === 9) {
-      return 10;
+      return hand.includes(10) ? 10 : 9;
     } else if (nextTarget === 10) {
-      return 9;
-  } else if (nextTarget === 11) {
-    return hand.includes(10) ? 10 : 12;
-  }
-  return 1;
+      return hand.includes(9) ? 9 : 10;
+    } else if (nextTarget === 11) {
+      return hand.includes(10) ? 10 : 12;
+    }
+  return hand[0];
 }
 }
   // We have 13 5 1 4 12, 6 7 8 9 
