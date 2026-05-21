c715c97d7c52263bbd2af407281419eb2701f523
AdamT2529 <profileytwow1@gmail.com>
2026-05-21 01:40:43 +0000

    done i think

--- src/ai/atom.jsx
+++ src/ai/atom.jsx
@@ -3,37 +3,37 @@ export const atomAi = {
   // icon: "https://www.sciencefacts.net/atom-2.html", // an image link
   getNextCard: (hand, targets, opponentPlays) => {
     let nextTarget = targets[targets.length - 1];
-    c0jst
-    if (nextTarget === 13) {
+    const pick = (a, b) =>
+      hand.includes(a) ? a :
+      hand.includes(b) ? b : 
+      hand[0];
+    
+      if (nextTarget === 13) {
       return hand.includes(13) ? 13 : hand[0]
     } else if (nextTarget === 5) {
         return hand.includes(4) ? 4 : hand[0]
       } else if (nextTarget === 1) {
-        return hand.includes(1) ? 1 : 2;
+        return pick (1, 2);
       } else if (nextTarget === 4) {
         return hand.includes(3) ? 3 : hand[0];
       } else if (nextTarget === 2) {
-        return hand.includes(2) ? 2 : 1;
+        return pick(2, 1);
       } else if (nextTarget === 3) {
-      return hand.includes(2) ? 2 : hand[0]
+      return pick(3, 4);
     } else if (nextTarget === 12) {
       return hand.includes(12) ? 12 : hand[0]
     } else if (nextTarget === 6) {
-      return hand.includes(5) ? 5 : 6;
+      return pick(6, 5);
     } else if (nextTarget === 7) {
-      return hand.includes(8) ? 8 : 7;
+      return pick(7, 6);
     } else if (nextTarget === 8) {
-      return hand.includes(7) 
-      ? 7
-      : hand.includes(8)
-       ? 8
-       : hand[0];
+      return pick(8, 7);
     } else if (nextTarget === 9) {
-      return hand.includes(10) ? 10 : 9;
+      return pick(9, 8);
     } else if (nextTarget === 10) {
-      return hand.includes(9) ? 9 : 10;
+      return pick(10, 9);
     } else if (nextTarget === 11) {
-      return hand.includes(10) ? 10 : 12;
+      return pick(11, 10);
     }
   return hand[0];
 }
